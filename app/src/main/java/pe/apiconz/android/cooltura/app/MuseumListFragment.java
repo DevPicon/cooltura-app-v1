867ab57851d46411d7db69a36d65bf6b3648ba9c
Armando <apiconz@gmail.com>
2014-12-18 01:56:25 -0500

    - added new Activity-Fragment for museum's detail - added intent

--- app/src/main/java/pe/apiconz/android/cooltura/app/MuseumListFragment.java
+++ app/src/main/java/pe/apiconz/android/cooltura/app/MuseumListFragment.java
@@ -1,10 +1,12 @@
 package pe.apiconz.android.cooltura.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MuseumListFragment extends Fragment {
+    private ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,10 +23,18 @@ public class MuseumListFragment extends Fragment {
 
         List<String> museumList = Arrays.asList("Museo de Arte Italiano","Museo de Arte de Lima", "Museo de Ciencias Naturales", "Museo de la Nación","Museo Arquelógico Rafael");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_museum,R.id.txtMuseumName,museumList);
+        adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_museum,R.id.txtMuseumName,museumList);
 
         ListView museumListView = (ListView)rootView.findViewById(R.id.listview_museums);
         museumListView.setAdapter(adapter);
+        museumListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String museum = adapter.getItem(position);
+                Intent intent = new Intent(getActivity(),MuseumDetailActivity.class).putExtra(Intent.EXTRA_TEXT, museum);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
