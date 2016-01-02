9c760ab7075d76f89f196bc4451bc36dd5f66fec
Armando <apiconz@gmail.com>
2016-01-02 03:25:20 -0500

    navigation drawer added

--- app/src/main/java/pe/apiconz/android/cooltura/app/presentation/activities/MainActivity.java
+++ app/src/main/java/pe/apiconz/android/cooltura/app/presentation/activities/MainActivity.java
@@ -1,13 +1,17 @@
 package pe.apiconz.android.cooltura.app.presentation.activities;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.Bind;
 import pe.apiconz.android.cooltura.app.R;
 import pe.apiconz.android.cooltura.app.presentation.fragments.AboutFragment;
 import pe.apiconz.android.cooltura.app.presentation.fragments.ShinPlaceListFragment;
@@ -15,6 +19,10 @@ import pe.apiconz.android.cooltura.app.presentation.fragments.ShinPlaceListFragm
 
 public class MainActivity extends BaseActivity {
 
+    @Bind(R.id.drawer_layout)
+    protected DrawerLayout drawerLayout;
+    @Bind(R.id.nav_view)
+    protected NavigationView navigationView;
     private String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -37,6 +45,18 @@ public class MainActivity extends BaseActivity {
     protected void onCreate() {
         Log.d(TAG, "Paso por onCreate");
         setSupportActionBar();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                item.setChecked(true);
+
+                drawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
 
 
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -62,13 +82,14 @@ public class MainActivity extends BaseActivity {
 
         switch (item.getItemId()) {
             case R.id.action_settings:
-
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
             case R.id.action_about:
-
                 DialogFragment newFragment = new AboutFragment();
                 newFragment.show(getSupportFragmentManager(), "");
+                return true;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
                 return true;
 
             default:
